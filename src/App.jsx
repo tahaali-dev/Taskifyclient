aabbabd5afd5726d4fa5433c6458cb8ccb32795d
devtahajs <tahaali.css@gmail.com>
2023-08-25 13:00:07 +0530

    create page test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AuthPage from "./Pages/AuthPage/AuthPage";
 import Dash from "./Pages/Dashboard/Dash";
 import Layout from "./Components/Layout/Layout";
 import SingleTask from "./Pages/SingleTask/SingleTask";
+import TaskCreation from "./Pages/TaskCreationPage/TaskCreation";
 // ------------------Imports----------------
 
 export default function App() {
@@ -11,9 +12,10 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route  element={<Layout />}>
+        <Route element={<Layout />}>
           <Route path="/dash" element={<Dash />} />
           <Route path="/single-task" element={<SingleTask />} />
+          <Route path="/create-new" element={<TaskCreation />} />
         </Route>
       </Routes>
     </BrowserRouter>
