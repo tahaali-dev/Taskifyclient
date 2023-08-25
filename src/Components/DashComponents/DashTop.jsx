58ad3acee437f5f42699a9cd1f72c1e1fece8114
devtahajs <tahaali.css@gmail.com>
2023-08-25 21:14:12 +0530

    dash test 1

--- src/Components/DashComponents/DashTop.jsx
+++ src/Components/DashComponents/DashTop.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Dash.css";
+import quotes from "../data.js";
+import { Link } from "react-router-dom";
 
 const DashTop = () => {
+  const [randomQuoteIndex, setRandomQuoteIndex] = useState(
+    Math.floor(Math.random() * quotes.length)
+  );
+
+  useEffect(() => {
+    const newRandomQuoteIndex = Math.floor(Math.random() * quotes.length);
+    setRandomQuoteIndex(newRandomQuoteIndex);
+  }, [quotes]);
+
+  let newQuote = quotes[randomQuoteIndex];
+
+  console.log(newQuote);
+
   return (
     <div className="dash-top">
-      <div className="dash-1 dash">Dash 1</div>
+      <div className="dash-1 ">
+        <div className="dash-1-left dash">
+          <h2>Taha Ali</h2>
+        </div>
 
-      <div className="dash-2 dash">Dash 2</div>
+        <div className="dash-1-right dash">
+          <h2>{newQuote.quote}</h2>
+          <p>{newQuote.author}</p>
+        </div>
+      </div>
 
-      <div className="dash-3 dash">Dash 3</div>
+      <div className="dash-2">
+        <div className="dash-2-left dash">
+          <h2>Your Task 📝 Journey = 100t</h2>
+        </div>
 
-      <div className="dash-4 dash">Dash 4</div>
+        <div className="dash-2-right dash">
+          <Link to="/create-new" className="linkd">
+            <i className="fa-solid fa-plus"></i>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/Components/data.js
@@ -0,0 +1,214 @@
+const motivationQuotes = [
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "Don't watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+  },
+  {
+    quote: "The future depends on what you do today.",
+    author: "Mahatma Gandhi",
+  },
+  {
+    quote: "The secret of getting ahead is getting started.",
+    author: "Mark Twain",
+  },
+  {
+    quote: "Your time is limited, don't waste it living someone else's life.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  },
+  // Add more quotes...
+
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "Don't watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+  },
+  {
+    quote: "The future depends on what you do today.",
+    author: "Mahatma Gandhi",
+  },
+  {
+    quote: "The secret of getting ahead is getting started.",
+    author: "Mark Twain",
+  },
+  {
+    quote: "Your time is limited, don't waste it living someone else's life.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  },
+  {
+    quote: "The journey of a thousand miles begins with one step.",
+    author: "Lao Tzu",
+  },
+  {
+    quote: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein",
+  },
+  {
+    quote: "You miss 100% of the shots you don't take.",
+    author: "Wayne Gretzky",
+  },
+  {
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+
+  {
+    quote: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "The journey of a thousand miles begins with one step.",
+    author: "Lao Tzu",
+  },
+  {
+    quote: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein",
+  },
+  {
+    quote: "You miss 100% of the shots you don't take.",
+    author: "Wayne Gretzky",
+  },
+
+  // ... (previous quotes)
+  {
+    quote: "The harder I work, the luckier I get.",
+    author: "Gary Player",
+  },
+  {
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    quote:
+      "In the midst of winter, I found there was, within me, an invincible summer.",
+    author: "Albert Camus",
+  },
+  {
+    quote: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "Your time is limited, don't waste it living someone else's life.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "The journey of a thousand miles begins with one step.",
+    author: "Lao Tzu",
+  },
+  {
+    quote: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein",
+  },
+  {
+    quote: "You miss 100% of the shots you don't take.",
+    author: "Wayne Gretzky",
+  },
+  {
+    quote: "The harder I work, the luckier I get.",
+    author: "Gary Player",
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+
+  {
+    quote: "Don't watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+  },
+  {
+    quote: "The future depends on what you do today.",
+    author: "Mahatma Gandhi",
+  },
+  {
+    quote: "The secret of getting ahead is getting started.",
+    author: "Mark Twain",
+  },
+  {
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  },
+  {
+    quote: "The journey of a thousand miles begins with one step.",
+    author: "Lao Tzu",
+  },
+  {
+    quote: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein",
+  },
+  {
+    quote: "You miss 100% of the shots you don't take.",
+    author: "Wayne Gretzky",
+  },
+
+  // ... (previous quotes)
+
+  {
+    quote: "You are never too old to set another goal or to dream a new dream.",
+    author: "C.S. Lewis",
+  },
+  {
+    quote:
+      "The best time to plant a tree was 20 years ago. The second best time is now.",
+    author: "Chinese Proverb",
+  },
+
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "The harder I work, the luckier I get.",
+    author: "Gary Player",
+  },
+  {
+    quote: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt",
+  },
+];
+
+export default motivationQuotes;
